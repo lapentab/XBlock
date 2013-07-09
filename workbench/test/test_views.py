2eaef55313e96402863b4a273e1a0f2331819bce
Sarina Canelake <sarina@mitx.mit.edu>
2013-07-09 15:50:24 -0400

    More pylint violations

--- workbench/test/test_views.py
+++ workbench/test/test_views.py
@@ -6,10 +6,11 @@ from django.test.client import Client
 from django.test import TestCase
 
 from mock import patch
+# Nose redefines assert_raises
+# pylint: disable=E0611
 from nose.tools import assert_raises
+# pylint: enable=E0611
 
-from workbench import scenarios
-from workbench.runtime import Usage
 from xblock.core import XBlock, String, Scope
 from xblock.fragment import Fragment
 
@@ -19,40 +20,44 @@ class TestMultipleViews(TestCase):
 
     class MultiViewXBlock(XBlock):
         """A bare-bone XBlock with two views."""
-        def student_view(self, context):
+        def student_view(self, _context):
+            """A view, with the default name."""
             return Fragment(u"This is student view!")
 
-        def another_view(self, context):
+        def another_view(self, _context):
+            """A secondary view for this block."""
             return Fragment(u"This is another view!")
 
     @patch('xblock.core.XBlock.load_class', return_value=MultiViewXBlock)
-    def test_multiple_views(self, mock_load_classes):
-        c = Client()
+    def test_multiple_views(self, _mock_load_classes):
+        client = Client()
 
         # The default view is student_view
-        response = c.get("/view/multiview/")
+        response = client.get("/view/multiview/")
         self.assertIn("This is student view!", response.content)
 
         # We can ask for student_view directly
-        response = c.get("/view/multiview/student_view/")
+        response = client.get("/view/multiview/student_view/")
         self.assertIn("This is student view!", response.content)
 
         # We can also ask for another view.
-        response = c.get("/view/multiview/another_view/")
+        response = client.get("/view/multiview/another_view/")
         self.assertIn("This is another view!", response.content)
 
 
 class XBlockWithHandlerAndStudentState(XBlock):
-
+    """A bare-bone XBlock with one view and one json handler."""
     the_data = String(default="def", scope=Scope.user_state)
 
-    def student_view(self, context):
+    def student_view(self, _context):
+        """Provide the default view."""
         body = u"The data: %r." % self.the_data
         body += u":::%s:::" % self.runtime.handler_url("update_the_data")
         return Fragment(body)
 
     @XBlock.json_handler
-    def update_the_data(self, data):
+    def update_the_data(self, _data):
+        """Mock handler that updates the student state."""
         self.the_data = self.the_data + "x"
         return {'the_data': self.the_data}
 
@@ -61,39 +66,41 @@ class XBlockWithHandlerAndStudentState(XBlock):
     'xblock.core.XBlock.load_class',
     return_value=XBlockWithHandlerAndStudentState
 )
-def test_xblock_with_handler_and_student_state(mock_load_class):
-    c = Client()
+def test_xblock_with_handler(_mock_load_class):
+    # Tests an XBlock that provides a handler, and has some simple
+    # student state
+    client = Client()
 
     # Initially, the data is the default.
-    response = c.get("/view/xblockwithhandlerandstudentstate/")
+    response = client.get("/view/xblockwithhandlerandstudentstate/")
     assert "The data: 'def'." in response.content
     parsed = response.content.split(':::')
     assert len(parsed) == 3
     handler_url = parsed[1]
 
     # Now change the data.
-    response = c.post(handler_url, "{}", "text/json")
+    response = client.post(handler_url, "{}", "text/json")
     the_data = json.loads(response.content)['the_data']
     assert the_data == "defx"
 
     # Change it again.
-    response = c.post(handler_url, "{}", "text/json")
+    response = client.post(handler_url, "{}", "text/json")
     the_data = json.loads(response.content)['the_data']
     assert the_data == "defxx"
 
 
 @patch('xblock.core.XBlock.load_class', return_value=XBlock)
-def test_xblock_without_handler_and_student_state(mock_load_class):
+def test_xblock_without_handler(_mock_load_class):
     # Test that an XBlock without a handler raises an Exception
     # when we try to hit a handler on it
-    c = Client()
+    client = Client()
     handler_url = "/handler/usage_175/update_the_data/?student=student_1"
 
     # The default XBlock implementation doesn't provide
     # a handler, so this call should raise an exception
     # (from xblock.runtime.Runtime.handle)
     with assert_raises(Exception):
-        c.post(handler_url, '{}', 'text/json')
+        client.post(handler_url, '{}', 'text/json')
 
 
 class XBlockWithoutStudentView(XBlock):
@@ -104,10 +111,10 @@ class XBlockWithoutStudentView(XBlock):
 
 
 @patch('xblock.core.XBlock.load_class', return_value=XBlockWithoutStudentView)
-def test_xblock_without_student_view(mock_load_class):
+def test_xblock_no_student_view(_mock_load_class):
     # Try to get a response. Will try to render via WorkbenchRuntime.render;
     # since no view is provided in the XBlock, will return a Fragment that
     # indicates there is no view available.
-    c = Client()
-    response = c.get("/view/xblockwithoutstudentview/")
+    client = Client()
+    response = client.get("/view/xblockwithoutstudentview/")
     assert 'No such view' in response.content
--- xblock/test/test_view_counter.py
+++ xblock/test/test_view_counter.py
@@ -1,17 +1,20 @@
 """ Simple test for the view counter that verifies that it is updating properly """
+# Nose redefines assert_in and assert_equals
+# pylint: disable=E0611
 from nose.tools import assert_in, assert_equals
+# pylint:enable=E0611
 from mock import Mock
 from xblock.view_counter import ViewCounter
 from xblock.runtime import DbModel
 from xblock.test import DictKeyValueStore
 from collections import namedtuple
 
-TestUsage = namedtuple('TestUsage', 'id, def_id')
+TESTUSAGE = namedtuple('TestUsage', 'id, def_id')
 
 
 def test_view_counter_state():
     key_store = DictKeyValueStore()
-    db_model = DbModel(key_store, ViewCounter, 's0', TestUsage('u0', 'd0'))
+    db_model = DbModel(key_store, ViewCounter, 's0', TESTUSAGE('u0', 'd0'))
     tester = ViewCounter(Mock(), db_model)
 
     assert_equals(tester.views, 0)
